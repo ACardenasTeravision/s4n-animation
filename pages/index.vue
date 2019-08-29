fefecd87a99f0f9a6cafb96a94deccf661c6ac75
Adrian Cardenas <acardenas@teravisiontech.com>
2019-08-28 19:05:12 -0500

    update

--- pages/index.vue
+++ pages/index.vue
@@ -17,44 +17,32 @@ const circunference = PI * 2;
 export default {
   data() {
     return {
-      radius: 70,
-      curr: 0,
-      start: 0,
-      finish: 360,
-      currentStep: 0,
-      currentSegment: {
-        width: 0,
-        start: 0,
-        end: 72,
-      }
+      currentStep: 0
     }
   },
   methods: {
-    animateCircleSegment3(segmentName, color, width, height, start, finish, draw_to) {
+    animateCircleSegment(segmentName, color, width, height, start, finish, draw_to) {
       const canvas = document.getElementById(segmentName);
       let centerX = width / 2;
       let centerY = height / 2;
       let radius = (width / 2) - 5;
-      console.log(radius);
       const ctx = canvas.getContext('2d');
       ctx.clearRect(0, 0, width, height);
       ctx.beginPath();
       ctx.strokeStyle = color;
       ctx.lineWidth = 5;
-      if(!draw_to) {
-        draw_to = start;
-      }
+      draw_to = !draw_to ? start : draw_to;
       ctx.arc(centerX, centerY, radius, start, draw_to, false);
       ctx.stroke();
 
       if (draw_to < finish) {
         requestAnimationFrame(() => {
-          this.animateCircleSegment3(segmentName, color, width, height, start, finish, draw_to + this.getRadians(2));
+          this.animateCircleSegment(segmentName, color, width, height, start, finish, draw_to + this.getRadians(2));
         });
       }
     },
     createAllCircles() {
-      this.createSegment('mainCircle', 0, 360, false);
+      this.createSegment('mainCircle', 150, 150, 0, 360, false);
       //this.createSegment('firstSegment', 0, 72, false);
       // this.createSegment('secondSegment', 72, 144, false);
       // this.createSegment('thirdSegment', 144, 288, false);
@@ -63,12 +51,12 @@ export default {
       // this.createSegment('firstFinalSegment', 288, 72, false);
       // this.createSegment('secondFinalSegment', 288, 72, true);
     },
-    createSegment(canvasName, from, to, clockwise) {
+    createSegment(canvasName, width, height, from, to, clockwise) {
       const canvas = document.getElementById(canvasName);
       const context = canvas.getContext('2d');
-      let centerX = canvas.width / 2;
-      let centerY = canvas.height / 2;
-      const radius = 70; //chequear esto
+      let centerX = width / 2;
+      let centerY = height / 2;
+      let radius = (width / 2) - 5;
 
       context.beginPath();
       context.arc(centerX, centerY, radius, this.getRadians(from), this.getRadians(to), clockwise);
@@ -84,8 +72,8 @@ export default {
   },
   mounted() {
     this.createAllCircles();
-    this.animateCircleSegment3('secondSegment', 'red', 150, 150, this.getRadians(0), this.getRadians(72));
-    setTimeout(() =>{ this.animateCircleSegment3('secondSegment', 'gray', 150, 150, this.getRadians(0), this.getRadians(72)) }, 5000);
+    this.animateCircleSegment('mainCircle', 'grey', 150, 150, this.getRadians(0), this.getRadians(360));
+    // setTimeout(() =>{ this.animateCircleSegment3('secondSegment', 'gray', 150, 150, this.getRadians(0), this.getRadians(72)) }, 5000);
   }
 }
 </script>
