2a9da39b6a8df33a75860b4da88ee2a73bc2dee6
Adrian Cardenas <acardenas@teravisiontech.com>
2019-08-29 20:24:18 -0500

    update

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,13 @@
     <div id="circle-container">
       <canvas id="mainCircle" width="150" height="150"/>
       <canvas id="firstSegment" width="150" height="150"/>
-      <canvas id="secondSegment" width="150" height="150"/>
+      <!-- <canvas id="secondSegment" width="150" height="150"/>
       <canvas id="thirdSegment" width="150" height="150"/>
-      <canvas id="fourthSegment" width="150" height="150"/>
+      <canvas id="fourthSegment" width="150" height="150"/> -->
     </div>
+    <h1>{{ currentStep }}</h1>
+    <button @click="this.increment">+</button>
+    <button @click="this.decrement">-</button>
   </div>
 </template>
 
@@ -17,41 +20,45 @@ const circunference = PI * 2;
 export default {
   data() {
     return {
-      currentStep: 0
+      currentStep: 0,
+      moveForward: true
     }
   },
   methods: {
-    animateCircleSegment(segmentName, color, width, height, start, finish, draw_to) {
+    increment() {
+      this.currentStep = this.currentStep + 1;
+      this.moveForward = true;
+    },
+    decrement() {
+      this.currentStep = this.currentStep - 1;
+      this.moveForward = false;
+    },
+    animateCircleSegment(segmentName, color, width, height, start, finish, anticlockwise, draw_to) {
       const canvas = document.getElementById(segmentName);
       let centerX = width / 2;
       let centerY = height / 2;
       let radius = (width / 2) - 5;
       const ctx = canvas.getContext('2d');
-      ctx.clearRect(0, 0, width, height);
+      //ctx.clearRect(0, 0, width, height);
       ctx.beginPath();
       ctx.strokeStyle = color;
       ctx.lineWidth = 5;
       draw_to = !draw_to ? start : draw_to;
-      ctx.arc(centerX, centerY, radius, start, draw_to, false);
+      ctx.arc(centerX, centerY, radius, start, draw_to, anticlockwise);
       ctx.stroke();
-
+      ctx.closePath();
       if (draw_to < finish) {
         requestAnimationFrame(() => {
-          this.animateCircleSegment(segmentName, color, width, height, start, finish, draw_to + this.getRadians(2));
+          anticlockwise ?
+            this.animateCircleSegment(segmentName, color, width, height, start, finish, anticlockwise, draw_to - this.getRadians(2)):
+            this.animateCircleSegment(segmentName, color, width, height, start, finish, anticlockwise, draw_to + this.getRadians(2))
         });
       }
     },
     createAllCircles() {
       this.createSegment('mainCircle', 150, 150, 0, 360, false);
-      //this.createSegment('firstSegment', 0, 72, false);
-      // this.createSegment('secondSegment', 72, 144, false);
-      // this.createSegment('thirdSegment', 144, 288, false);
-      // this.createSegment('fourthSegment', 288, 360, false);
-
-      // this.createSegment('firstFinalSegment', 288, 72, false);
-      // this.createSegment('secondFinalSegment', 288, 72, true);
     },
-    createSegment(canvasName, width, height, from, to, clockwise) {
+    createSegment(canvasName, width, height, from, to, anticlockwise) {
       const canvas = document.getElementById(canvasName);
       const context = canvas.getContext('2d');
       let centerX = width / 2;
@@ -59,7 +66,7 @@ export default {
       let radius = (width / 2) - 5;
 
       context.beginPath();
-      context.arc(centerX, centerY, radius, this.getRadians(from), this.getRadians(to), clockwise);
+      context.arc(centerX, centerY, radius, this.getRadians(from), this.getRadians(to), anticlockwise);
       context.fillStyle = 'transparent';
       context.fill();
       context.lineWidth = 5;
@@ -72,8 +79,69 @@ export default {
   },
   mounted() {
     this.createAllCircles();
-    this.animateCircleSegment('mainCircle', 'grey', 150, 150, this.getRadians(0), this.getRadians(360));
+    //this.animateCircleSegment('mainCircle', 'red', 150, 150, this.getRadians(144), this.getRadians(288));
     // setTimeout(() =>{ this.animateCircleSegment3('secondSegment', 'gray', 150, 150, this.getRadians(0), this.getRadians(72)) }, 5000);
+  },
+  watch: {
+    currentStep: function (val) {
+      if(val < 0) {
+        this.currentStep = 0;
+      } else if (val > 7) {
+        this.currentStep = 8;
+      } else {
+        switch (val) {
+          case 0:
+            if(this.moveForward == false) {
+              console.log('ANIMANDO HACIA EL PASO 0');
+              this.animateCircleSegment('firstSegment', 'orange', 150, 150, this.getRadians(72), this.getRadians(0), true);
+            }
+          break;
+          case 1:
+            console.log('STEP', this.currentStep);
+            console.log('DIRECCION', this.moveForward);
+            console.log('ANIMANDO HACIA EL PASO 1');
+            this.animateCircleSegment('firstSegment', 'red', 150, 150, this.getRadians(0), this.getRadians(72), false);
+          break;
+          case 2:
+            console.log('STEP', this.currentStep);
+            console.log('DIRECCION', this.moveForward);
+            console.log('ANIMANDO HACIA EL PASO 2');
+            this.animateCircleSegment('firstSegment', 'gray', 150, 150, this.getRadians(0), this.getRadians(72), false);
+          break;
+          case 3:
+            console.log('STEP', this.currentStep);
+            console.log('DIRECCION', this.moveForward);
+            console.log('ANIMANDO HACIA EL PASO 3');
+            this.animateCircleSegment('firstSegment', 'red', 150, 150, this.getRadians(72), this.getRadians(144), false);
+
+          break;
+          case 4:
+            console.log('STEP', this.currentStep);
+            console.log('DIRECCION', this.moveForward);
+            console.log('ANIMANDO HACIA EL PASO 4');
+            this.animateCircleSegment('firstSegment', 'gray', 150, 150, this.getRadians(72), this.getRadians(144), false);
+          break;
+          case 5:
+            console.log('STEP', this.currentStep);
+            console.log('DIRECCION', this.moveForward);
+            console.log('ANIMANDO HACIA EL PASO 5');
+            this.animateCircleSegment('firstSegment', 'red', 150, 150, this.getRadians(144), this.getRadians(288), false);
+          break;
+          case 6:
+            console.log('STEP', this.currentStep);
+            console.log('DIRECCION', this.moveForward);
+            console.log('ANIMANDO HACIA EL PASO 6');
+            this.animateCircleSegment('firstSegment', 'gray', 150, 150, this.getRadians(144), this.getRadians(288), false);
+          break;
+          case 7:
+            console.log('STEP', this.currentStep);
+            console.log('DIRECCION', this.moveForward);
+            console.log('ANIMANDO HACIA EL PASO 7');
+            this.animateCircleSegment('firstSegment', 'red', 150, 150, this.getRadians(288), this.getRadians(360), false);
+          break;
+        }
+      }
+    }
   }
 }
 </script>
@@ -86,7 +154,6 @@ export default {
   }
 
   canvas  {
-    border: 1px solid;
     position: absolute;
     transform: rotate(-90deg);
   }
